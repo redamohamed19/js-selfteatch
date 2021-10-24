7e9b7f3af89f8e1d7a35bf9c653df6561db4e52a
Sayah Reda <reda1998.rs@gmail.com>
2021-10-24 11:49:49 +0100

    smallest common multipe

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
-function sumPrimes(num) {
-  var prime_numbers=[];
-  var sum=0;
-  function isPrime(num){
-    for(let i=2;i<=Math.sqrt(num);i++){
-      if(num%i==0){
+function smallestCommons(arr) {
+  var min, max;
+  if (arr[0] > arr[1]) {
+    max = arr[0];
+    min = arr[1];
+  } else {
+    max = arr[1];
+    min = arr[0];
+  }
+  var sub = max - min + 1;
+  var duplicate_array = [];
+  function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0) {
         return false;
       }
-      
     }
     return true;
   }
- for(let i=2;i<=num;i++){
-   if(isPrime(i)){
-     sum=sum+i;
-   }
- }
- console.log(sum)
- return sum;
+  for (let i = max; i < 100000000; i++) {
+    var check = 0;
+    for (let j = min; j <= max; j++) {
+      if (i % j == 0) {
+        check++;
+      }
+    }
+    if (check == sub) {
+      console.log(i);
+      return i;
+      break;
+    }
+  }
 }
 
-sumPrimes(977);
+smallestCommons([5, 1]);
