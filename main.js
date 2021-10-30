3e25f239bdb71193455614dce0089ecbd7eecca4
Sayah Reda <reda1998.rs@gmail.com>
2021-10-30 22:39:12 +0100

    everything be true

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-function binaryAgent(str) {
-  var tabStr = [];
-  var sentence = [];
-  tabStr = str.split(' ');
-  for (let i = 0; i < tabStr.length; i++) {
-    sentence.push(String.fromCharCode(parseInt(tabStr[i], 2)));
+function truthCheck(collection, pre) {
+  for (let col of collection) {
+    if (!col[pre]) {
+      return false;
+    }
+
+    console.log(col[pre]);
   }
-  var fin = sentence.join('');
-  return fin;
+  return true;
 }
 
-binaryAgent(
-  '01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111'
+truthCheck(
+  [
+    { user: 'Tinky-Winky', sex: 'male' },
+    { user: 'Dipsy', sex: 'male' },
+    { user: 'Laa-Laa', sex: 'female' },
+    { user: 'Po', sex: 'female' }
+  ],
+  'sex'
 );
