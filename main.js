06faeceac291afd86b97f92325ccf0296546cce4
Sayah Reda <reda1998.rs@gmail.com>
2021-10-31 11:40:41 +0100

    make a person*

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-function truthCheck(collection, pre) {
-  for (let col of collection) {
-    if (!col[pre]) {
-      return false;
-    }
+const Person = function(firstAndLast) {
+  // Only change code below this line
+   this.getFirstName = function() {
+     var name=firstAndLast.split(" ");
+    return name[0];
+  };
+     this.getLastName = function() {
+     var name=firstAndLast.split(" ");
+    return name[1];
+  };
+       this.setFirstName = function(fname) {
+     var name=firstAndLast.split(" ");
+     name[0]=fname;
+    firstAndLast= name.join(" ");
+  };
+         this.setLastName = function(lname) {
+     var name=firstAndLast.split(" ");
+     name[1]=lname;
+    firstAndLast= name.join(" ");
+  };
+           this.setFullName = function(flname) {
+    firstAndLast=flname;
+  };
+  // Complete the method below and implement the others similarly
+  this.getFullName = function() {
+    return firstAndLast;
+  };
+  return firstAndLast;
+};
 
-    console.log(col[pre]);
-  }
-  return true;
-}
-
-truthCheck(
-  [
-    { user: 'Tinky-Winky', sex: 'male' },
-    { user: 'Dipsy', sex: 'male' },
-    { user: 'Laa-Laa', sex: 'female' },
-    { user: 'Po', sex: 'female' }
-  ],
-  'sex'
-);
+const bob = new Person('Bob Ross');
+bob.setFirstName("Haskell")
+bob.getFullName();
